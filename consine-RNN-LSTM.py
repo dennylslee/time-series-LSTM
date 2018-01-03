cfa084462dad6ccb2c0e6f6006959f0b32dd97ed
dennylslee <dennylslee@hotmail.com>
2018-01-03 18:30:39 -0500

    rerun the results

--- consine-RNN-LSTM.py
+++ consine-RNN-LSTM.py
@@ -1,4 +1,6 @@
-# LSTM for international airline passengers problem with regression framing
+# Use Keras LSTM for performing time series forecasting
+# Tensorflow needs to be pre installed before utilizing Keras
+# 
 import numpy
 import matplotlib.pyplot as plt
 from pandas import read_csv
@@ -10,7 +12,6 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
 # convert an array of values into a dataset matrix
-#def create_dataset(dataset, look_back=1):
 def create_dataset(dataset, look_back):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-look_back-1):
@@ -31,7 +32,8 @@ scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 
 # split into train and test sets
-train_size = int(len(dataset) * 0.20)
+# control the proportion of training set here
+train_size = int(len(dataset) * 0.02)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 
@@ -48,33 +50,39 @@ testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1],1))
 
 # create and fit the LSTM network
 # single hidden layer (timestep is one) and cell states
-# The "unit" value in this case is the size of the cell state and the size of the hidden state bei
+# The "unit" value in this case is the size of the cell state and the size of the hidden state
 model = Sequential()
 model.add(LSTM(10, input_shape=(look_back,1))) 	# NOTE: time steps and features are reversed from example given
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(trainX, trainY, epochs=50, batch_size=1, verbose=2)
+
 # make predictions
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
+
 # invert predictions
 trainPredict = scaler.inverse_transform(trainPredict)
 trainY = scaler.inverse_transform([trainY])
 testPredict = scaler.inverse_transform(testPredict)
 testY = scaler.inverse_transform([testY])
+
 # calculate root mean squared error
 trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
+
 # shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
 trainPredictPlot[:, :] = numpy.nan
 trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
 # shift test predictions for plotting
 testPredictPlot = numpy.empty_like(dataset)
 testPredictPlot[:, :] = numpy.nan
 testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
 # plot baseline and predictions
 plt.plot(scaler.inverse_transform(dataset))
 plt.plot(trainPredictPlot)
